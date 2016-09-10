766fd43ca1d159fa027ffea7f44c39ddbb502fe3
Danila Chenchik <chenchik@Danilas-MacBook-Pro.local>
2016-09-10 16:19:10 -0400

    init added simple mvc

---
+++ simple-mvc-master/app/controllers/Home.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * The default home controller, called when no controller/method has been passed
+ * to the application.
+ */
+class Home extends Controller
+{
+    /**
+     * The default controller method.
+     *
+     * @return void
+     */
+    public function index($name = 'alex', $mood = 'normal')
+    {
+        $user = $this->model('user');
+        $user->name = $name;
+
+        $this->view('home/index', [
+            'name' => $user->name,
+            'mood' => $mood
+        ]);
+    }
+}
