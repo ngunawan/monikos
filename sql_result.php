68e06cd58488787ba2232749a79e3a688d6530c2
Danila Chenchik <chenchik@Danilas-MacBook-Pro.local>
2016-09-12 18:58:17 -0400

    trying with indication

--- sql_result.php
+++ sql_result.php
@@ -8,13 +8,14 @@ $pass = "monikosdbpw";
 $dbname = "monikosdb";
 $conn = new mysqli($host, $dbuser, $pass, $dbname);
 
-$result = $conn->query("SELECT Generic,Brand FROM Drug");
+$result = $conn->query("SELECT Generic,Brand,Class FROM Drug");
 
 $outp = "";
 while($rs = $result->fetch_array(MYSQLI_ASSOC)) {
     if ($outp != "") {$outp .= ",";}
     $outp .= '{"Generic":"'  . $rs["Generic"] . '",';
     //$outp .= '"Brand":"'   . $rs["Brand"]        . '",';
+    $outp .= '"Class":"'   . $rs["Class"]        . '",';
     //$outp .= '"Indication":"'. $rs["Indication"]     . '"}';
     $outp .= '"Brand":"'. $rs["Brand"]     . '"}';
      //$outp .= '{"username":"'. $rs["username"] . '",';
