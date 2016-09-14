a6b8ee54cd255da365fde91f15a2ea17cbab820b
Danila Chenchik <chenchik@email.unc.edu>
2016-09-14 15:16:58 -0400

    removed a few plus signs in database

--- sql_result.php
+++ sql_result.php
@@ -8,16 +8,18 @@ $pass = "monikosdbpw";
 $dbname = "monikosdb";
 $conn = new mysqli($host, $dbuser, $pass, $dbname);
 
-$result = $conn->query("SELECT Generic,Brand FROM Drug");
+$result = $conn->query("SELECT Generic,Brand, Class, Indication, HintLikes, HintDislikes FROM Drug");
 
 $outp = "";
 while($rs = $result->fetch_array(MYSQLI_ASSOC)) {
     if ($outp != "") {$outp .= ",";}
     $outp .= '{"Generic":"'  . $rs["Generic"] . '",';
-    //$outp .= '"Brand":"'   . $rs["Brand"]        . '",';
-    //$outp .= '"Class":"'   . $rs["Class"]        . '",';
+    $outp .= '"Brand":"'   . $rs["Brand"]        . '",';
+    $outp .= '"Class":"'   . $rs["Class"]        . '",';
+    $outp .= '"Indication":"'   . $rs["Indication"]        . '",';
+    $outp .= '"HintLikes":"'   . $rs["HintLikes"]        . '",';
     //$outp .= '"Indication":"'. $rs["Indication"]     . '"}';
-    $outp .= '"Brand":"'. $rs["Brand"]     . '"}';
+    $outp .= '"HintDislikes":"'. $rs["HintDislikes"]     . '"}';
      //$outp .= '{"username":"'. $rs["username"] . '",';
      //$outp .= '"email":"'. $rs["email"]     . '"}';
 }
