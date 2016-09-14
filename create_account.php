ea8584ef6f226c827a651fdea0442942f342f78b
Danila Chenchik <chenchik@email.unc.edu>
2016-09-14 14:18:17 -0400

    added init create account

---
+++ create_account.php
@@ -0,0 +1,29 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+$host = "monikosdb.ci7ganrx1sxe.us-east-1.rds.amazonaws.com:3306";
+$dbuser = "monikosdbun";
+$pass = "monikosdbpw";
+$dbname = "monikosdb";
+$conn = new mysqli($host, $dbuser, $pass, $dbname);
+
+//$result = $conn->query("SELECT Generic,Brand FROM Dr);
+//$result = $conn->query("INSERT INTO Users (id, username, email, password) VALUES (NULL, ".$username.",". $email .",". $password .");");
+$result = $conn->query("INSERT INTO Users (id, username, email, password) VALUES (NULL, wow, wow@wow.com, dawg);");
+/*$outp = "";
+while($rs = $result->fetch_array(MYSQLI_ASSOC)) {
+    if ($outp != "") {$outp .= ",";}
+    $outp .= '{"Generic":"'  . $rs["Generic"] . '",';
+    //$outp .= '"Brand":"'   . $rs["Brand"]        . '",';
+    //$outp .= '"Class":"'   . $rs["Class"]        . '",';
+    //$outp .= '"Indication":"'. $rs["Indication"]     . '"}';
+    $outp .= '"Brand":"'. $rs["Brand"]     . '"}';
+     //$outp .= '{"username":"'. $rs["username"] . '",';
+     //$outp .= '"email":"'. $rs["email"]     . '"}';
+}
+$outp ='{"records":['.$outp.']}';*/
+$conn->close();
+
+echo($result);
+?>
