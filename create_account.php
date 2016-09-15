c2c93d853da8031c6ad3eb2412ea3b37a7706cc1
Danila Chenchik <chenchik@email.unc.edu>
2016-09-15 16:28:22 -0400

    added account add

--- create_account.php
+++ create_account.php
@@ -2,27 +2,13 @@
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 
-$posts = var_dump($_REQUEST);
-echo $posts;
-
-$username = $_REQUEST['username'];
-echo "username is " . $username;
-
-
-
+//connect to database
 $host = "monikosdb.ci7ganrx1sxe.us-east-1.rds.amazonaws.com:3306";
 $dbuser = "monikosdbun";
 $pass = "monikosdbpw";
 $dbname = "monikosdb";
 $conn = new mysqli($host, $dbuser, $pass, $dbname);
 
-//$result = $conn->query("SELECT Generic,Brand FROM Dr);
-//$result = $conn->query("INSERT INTO Users (id, username, email, password) VALUES (NULL, ".$username.",". $email .",". $password .");");
-//$result = $conn->query("INSERT INTO Users (id, username, email, password) VALUES (NULL, wow, wow@wow.com, dawg);");
-
-// Create connection
-//$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
@@ -33,30 +19,13 @@ VALUES (NULL, '".$_POST["username"]."', '".$_POST["email"]."', '".$_POST["passwo
 if ($conn->query($sql) === TRUE) {
     echo '[{
     "response": 200,
-    "username": "john",
-    "email": "john@john.com",
-    "password": "kkkjnkd"}]';
+    "username": "'.$_POST["username"].'",
+    "email": "'.$_POST["email"].'",
+    "password": "'.$_POST["password"].'"}]';
 } else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
+    echo '[{"response":"'.$conn->error.'"}]';
 }
 
 $conn->close();
-
-
-
-/*$outp = "";
-while($rs = $result->fetch_array(MYSQLI_ASSOC)) {
-    if ($outp != "") {$outp .= ",";}
-    $outp .= '{"Generic":"'  . $rs["Generic"] . '",';
-    //$outp .= '"Brand":"'   . $rs["Brand"]        . '",';
-    //$outp .= '"Class":"'   . $rs["Class"]        . '",';
-    //$outp .= '"Indication":"'. $rs["Indication"]     . '"}';
-    $outp .= '"Brand":"'. $rs["Brand"]     . '"}';
-     //$outp .= '{"username":"'. $rs["username"] . '",';
-     //$outp .= '"email":"'. $rs["email"]     . '"}';
-}
-$outp ='{"records":['.$outp.']}';*/
-//$conn->close();
-
 echo($result);
 ?>
