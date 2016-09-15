6e5fadf5b0941eba912e839a49d79c3b30f1fba0
Danila Chenchik <chenchik@email.unc.edu>
2016-09-14 20:43:45 -0400

    added alt sql query file

--- sql_result_test_db.php
+++ sql_result_test_db.php
@@ -3,17 +3,18 @@ header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 
 //alt database
-/*$host = "phpmyadmin.ci7ganrx1sxe.us-east-1.rds.amazonaws.com:3306";
+$host = "phpmyadmin.ci7ganrx1sxe.us-east-1.rds.amazonaws.com:3306";
 $dbuser = "phpMyAdmin";
 $pass = "phpMyAdmin";
 $dbname = "phpMyAdminDatabase";
-*/
 
 
+/*
 $host = "monikosdb.ci7ganrx1sxe.us-east-1.rds.amazonaws.com:3306";
 $dbuser = "monikosdbun";
 $pass = "monikosdbpw";
 $dbname = "monikosdb";
+*/
 $conn = new mysqli($host, $dbuser, $pass, $dbname);
 
 $result = $conn->query("SELECT Generic,Brand, Class, Indication, HintLikes, HintDislikes FROM Drug");
