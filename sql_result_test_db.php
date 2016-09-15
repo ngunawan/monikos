359ef5d89d01cd749c0b6ced184fd2d132e7b9e2
Danila Chenchik <chenchik@email.unc.edu>
2016-09-14 22:59:37 -0400

    init php cleaned up sql

--- sql_result_test_db.php
+++ sql_result_test_db.php
@@ -19,9 +19,21 @@ $conn = new mysqli($host, $dbuser, $pass, $dbname);
 
 $result = $conn->query("SELECT Generic,Brand, Class, Indication, HintLikes, HintDislikes FROM Drug");
 
+
+function clean($string) {
+   $string = str_replace(' ', '-', $string); // Replaces all spaces with hyphens.
+
+   return preg_replace('/[^A-Za-z0-9\-]/', '', $string); // Removes special chars.
+}
+
+
 $outp = "";
 while($rs = $result->fetch_array(MYSQLI_ASSOC)) {
     if ($outp != "") {$outp .= ",";}
+    $rs["Generic"] = clean($rs["Generic"]);
+    $rs["Brand"] = clean($rs["Brand"]);
+    $rs["Class"] = clean($rs["Class"]);
+    $rs["Indication"] = clean($rs["Indication"]);
     $outp .= '{"Generic":"'  . $rs["Generic"] . '",';
     $outp .= '"Brand":"'   . $rs["Brand"]        . '",';
     $outp .= '"Class":"'   . $rs["Class"]        . '",';
