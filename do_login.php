ce6833d713dcce91af356742150605b3d272c1ac
Danila Chenchik <chenchik@email.unc.edu>
2016-09-15 22:26:22 -0400

    kinda worked wit w3

--- do_login.php
+++ do_login.php
@@ -82,7 +82,7 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+        echo "id: " . $row["id"]. " - Username: " . $row["username"]. " Email: " . $row[" email: "]. "<br>";
     }
 } else {
     echo "0 results";
