92780cfeede8b063930e70fbaa9cf475de331c60
Danila Chenchik <chenchik@email.unc.edu>
2016-09-15 22:28:59 -0400

    trying to select wowee

--- do_login.php
+++ do_login.php
@@ -76,13 +76,13 @@ AND  `password` LIKE  'testpw'";
 		"error":"'.mysqli_connect_errno() . PHP_EOL.'"}]';
 }*/
 
-$sql = "SELECT * FROM Users";
+$sql = "SELECT * FROM Users WHERE username LIKE 'wowee'";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo "id: " . $row["id"]. " - Username: " . $row["username"]. " Email: " . $row[" email: "]. "<br>";
+        echo "id: " . $row["id"]. " - Username: " . $row["username"]. " Email: " . $row["email"]. "<br>";
     }
 } else {
     echo "0 results";
