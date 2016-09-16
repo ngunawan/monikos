54d6ade2a3e1fcb0186cfd0e9118bad5479548f2
Danila Chenchik <chenchik@email.unc.edu>
2016-09-15 22:34:49 -0400

    maybe sucesful

--- do_login.php
+++ do_login.php
@@ -76,7 +76,7 @@ AND  `password` LIKE  'testpw'";
 		"error":"'.mysqli_connect_errno() . PHP_EOL.'"}]';
 }*/
 
-$sql = "SELECT * FROM Users WHERE username LIKE 'wowee' AND password LIKE 'testpw'";
+$sql = "SELECT * FROM Users WHERE username LIKE '".$_POST["username"]."' AND password LIKE '".$_POST["password"]."'";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
