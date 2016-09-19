d558851fc9510287018b6c3ac4b7a717762a18ac
Danila Chenchik <chenchik@email.unc.edu>
2016-09-19 00:15:53 -0400

    trying to put real names in

--- create_list.php
+++ create_list.php
@@ -13,10 +13,10 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
-var_dump($_POST);
+//var_dump($_POST);
 
 $sql = "INSERT INTO Lists (lid, uid, name, drugids, drugnames)
-VALUES (NULL, '".$_COOKIE["user_id"]."', '".$_POST["name"]."', 'nkjnkjn', 'kjkjkj')";
+VALUES (NULL, '".$_COOKIE["user_id"]."', '".$_POST["name"]."', '11', '".$_POST["drugs"]."')";
 
 if ($conn->query($sql) === TRUE) {
     echo '[{
