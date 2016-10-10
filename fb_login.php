f093be86c9bf41c075ea96413219325803fb3ae6
Danila Chenchik <chenchik@email.unc.edu>
2016-10-10 00:53:08 -0400

    trying sql fb

--- fb_login.php
+++ fb_login.php
@@ -12,10 +12,21 @@ $conn = new mysqli($host, $dbuser, $pass, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
-echo '[{
+/*echo '[{
     "response": 200,
     "username": "'.$_POST["un"].'",
     "fbid": "'.$_POST["id"].'"}]';
+*/
+$sql = "SELECT * FROM Users WHERE id LIKE '".$profile["id"]."'";
+if ($conn->query($sql) === TRUE) {
+	echo '[{
+	"response": 200,
+	"username": "'.$_POST["username"].'",
+	"email": "'.$_POST["email"].'",
+	"password": "'.$_POST["password"].'"}]';
+} else {
+	echo '[{"response":"'.$conn->error.'"}]';
+}
 /*$sql = "INSERT INTO Users (id, username, email, password)
 VALUES (NULL, '".$_POST["username"]."', '".$_POST["email"]."', '".$_POST["password"]."')";
 
