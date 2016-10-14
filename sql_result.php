dfe4a9d8d44848142f2d20e37bd7506267857dec
Danila Chenchik <chenchik@email.unc.edu>
2016-10-14 00:18:02 -0400

    trying metis db

--- sql_result.php
+++ sql_result.php
@@ -3,18 +3,19 @@ header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 
 //alt database
-/*$host = "phpmyadmin.ci7ganrx1sxe.us-east-1.rds.amazonaws.com:3306";
-$dbuser = "phpMyAdmin";
-$pass = "phpMyAdmin";
-$dbname = "phpMyAdminDatabase";
-*/
+$host = "metis.ci7ganrx1sxe.us-east-1.rds.amazonaws.com:3306";
+$dbuser = "chenchik";
+$pass = "Legendary55";
+$dbname = "metis";
 
 
+/*
 $host = "monikosdb.ci7ganrx1sxe.us-east-1.rds.amazonaws.com:3306";
 $dbuser = "monikosdbun";
 $pass = "monikosdbpw";
 $dbname = "monikosdb";
 $conn = new mysqli($host, $dbuser, $pass, $dbname);
+*/
 
 //$result = $conn->query("SELECT DrugId, Generic,Brand, Class, Indication, HintLikes, HintDislikes FROM Drug");
 $result = $conn->query("SELECT * FROM Drug");
