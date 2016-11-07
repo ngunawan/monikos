e12082da330f7eb8ecac8f3e281f3769561ac190
Nik Gunawan <ngunawan@live.unc.edu>
2016-11-06 19:53:21 -0500

    nik's database commits

---
+++ db/email_hint.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html
+PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!-- 
+This is a skeleton html file that you can use to get you started on each new 
+HTML project
+
+Name: Your Name Here
+Class: CIS 3303
+Section: x
+-->
+
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="/mvc/public/css/main.css">
+
+</head>
+
+
+
+<body class='email-page'>
+
+
+<?php
+                                    $name = $_POST['name'];
+                                    $drug = $_POST['drug'];
+                                    $mnemonic = $_POST['mnemonic'];
+                                    $from = 'From: Monikos'; 
+                                    $to = 'nikkig2223@gmail.com'; 
+                                    $subject = 'New Mnemonic Suggestion';
+
+                                    $body = "From: $name\n Message:\n $mnemonic Drug: $drug";
+
+                                    if ($_POST['submit']) {				 
+                                        if (mail ($to, $subject, $body, $from)) { 
+        
+                                            
+    
+    echo '<p>Your suggestion has been sent!</p>';
+                                        } else { 
+                                            echo '<p>Something went wrong, go back and try again!</p>'; 
+                                        } 
+                                    }
+                                    ?>
+    
+    <a class=back-btn href=../mvc/public/home/drugDatabase>Back</a>
+    
+    </body>
+    
+    </html>
